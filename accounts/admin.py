55ee609686728d75c1d32058887a2e03113651ed
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-09-22 17:01:14 +0330

    create CustomUserAdmin: Admin's interact with CustomUser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('nat_id', 'username', 'email', )
+    ordering = ('-last_login', )
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('nat_id',)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "nat_id", "email", "password1", "password2", ),
+            },
+        ),
+    )

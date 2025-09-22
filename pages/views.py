e6fa4ed572720a93ff60b207a3c3d9d3f4a9810d
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-09-22 13:53:11 +0330

    impelement pages/urls and create html files

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomePageView(generic.TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(generic.TemplateView):
+    template_name = 'pages/about.html'
+
+class ContactPageView(generic.TemplateView):
+    template_name = 'pages/contact.html'

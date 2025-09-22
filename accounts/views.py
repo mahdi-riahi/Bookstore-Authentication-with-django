a13a5340a13198f4d339596aeea0416c0c5a54fe
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-09-22 15:08:52 +0330

    add signup and login and authentication urls. create signup view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+class SignUpView(generic.CreateView):
+    template_name = 'registration/signup.html'
+    form_class = UserCreationForm
+    success_url = 'home'

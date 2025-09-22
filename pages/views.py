3fdf813a29c5e3ca07b5abfb7497c4bd2cfae8fb
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-09-22 16:11:16 +0330

    activate navbar items

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,25 @@ from django.views import generic
 class HomePageView(generic.TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'home'
+        return context
+
+
 class AboutPageView(generic.TemplateView):
     template_name = 'pages/about.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'about'
+        return context
+
+
 class ContactPageView(generic.TemplateView):
     template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['active_page'] = 'contact'
+        return context
